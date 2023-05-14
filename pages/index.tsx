933752057c48d488261256cb850c091ef10bf500
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2023-05-13 20:14:16 -0500

    add share feat

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import MemeImage from "@/components/MemeImage";
 import Navbar from "@/components/Navbar";
 import TextInput from "@/components/TextInput";
 import Upload from "@/components/Upload";
-import html2canvas from "html2canvas";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import * as htmlToImage from "html-to-image";
@@ -10,7 +9,7 @@ import YSlider from "@/components/YSlider";
 import FontSlider from "@/components/FontSlider";
 
 export default function Home() {
-  const [image, setImage] = useState<string | null>(""); // "/soyjak6 1.png
+  const [image, setImage] = useState<string | null>("");
   const [text, setText] = useState("");
 
   const [textPosition, setTextPosition] = useState(0);
@@ -22,7 +21,6 @@ export default function Home() {
 
   useEffect(() => {
     setCanvasHeight(document.getElementById("capture")?.clientHeight || 0);
-    console.log(canvasHeight);
   }, [windowWidth, image]);
 
   const handleSliderChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,6 +38,31 @@ export default function Home() {
     window.addEventListener("resize", handleWindowResize);
     return () => window.removeEventListener("resize", handleWindowResize);
   }, []);
+
+  const shareImage = () => {
+    htmlToImage
+      .toBlob(document.getElementById("capture") as HTMLElement)
+      .then(function (dataUrl) {
+        if (navigator.share) {
+          navigator
+            .share({
+              files: [
+                new File([dataUrl as BlobPart], "meme.png", {
+                  type: "image/png",
+                }),
+              ],
+            })
+            .then(() => {
+              console.log("Image shared successfully");
+            })
+            .catch((error) => {
+              console.error("Error sharing image:", error);
+            });
+        } else {
+          console.log("Sharing not supported on this device");
+        }
+      });
+  };
 
   return (
     <div className="h-fit bg-black p-6">
@@ -66,16 +89,7 @@ export default function Home() {
           <TextInput memeText={text} setMemeText={setText} />
           <button
             className="flex w-full flex-col items-center rounded-xl bg-[#0F77FF] px-6 py-4 text-2xl font-bold text-[#FAFAFA] transition duration-200 hover:bg-[#0F77FFb3] hover:shadow-lg"
-            onClick={() => {
-              htmlToImage
-                .toJpeg(document.getElementById("capture") as HTMLElement)
-                .then(function (dataUrl) {
-                  var link = document.createElement("a");
-                  link.download = "meme.jpeg";
-                  link.href = dataUrl;
-                  link.click();
-                });
-            }}
+            onClick={shareImage}
           >
             Share
           </button>
