6b97b20f20d1ee0999fc2efeb74216772f8b2e66
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2023-05-13 20:33:22 -0500

    make normal

--- components/MemeImage.tsx
+++ components/MemeImage.tsx
@@ -1,3 +1,4 @@
+import { color } from "html2canvas/dist/types/css/types/color";
 import Image from "next/image";
 import React from "react";
 
@@ -28,11 +29,22 @@ function MemeImage({
             />
           )}
           <p
+            className="absolute w-full text-center text-stroke"
+            style={{ top: `${textPosition}px`, fontSize: `${fontSize}px` ,
+            WebkitTextStroke: `${fontSize/25}px black`,
+            color: 'white'
+
+          
+          }}
+          >
+            {memeText}
+          </p>
+          {/* <p
             className="absolute w-full bg-black/50 text-center text-white"
             style={{ top: `${textPosition}px`, fontSize: `${fontSize}px` }}
           >
             {memeText}
-          </p>
+          </p> */}
         </div>
       </div>
     </div>
